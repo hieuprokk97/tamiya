85fa0fe23d64004430b1d21440261ffde736c1b6
thinhtp <tranphuocthinh2001@gmail.com>
2023-12-29 16:32:57 +0700

    .

--- src/logic/getUser.js
+++ src/logic/getUser.js
@@ -4,6 +4,7 @@ const getUser = async (serverURL) => {
   try{
     const response = await fetch(`${serverURL}/user/all_users`);
     const data = await response.json();
+    console.log("🚀 ~ response:", response)
     return data.data
   }catch(error){
     console.log("Error: ", error)
